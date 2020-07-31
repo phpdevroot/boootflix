d129150d5d1d29566ed5bf0a26b332b0f458f93c
phpdevroot <phpdevroot@gmail.com>
2020-07-30 21:24:50 -0300

    Aula 03 parcial Ret 43:18

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,99 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias]= useState(['Teste']);
+  //const [nomeDaCategoria, setValues] = useState('Filmes');
+  
+  const valoresIniciais = {
+      nome:'Nome Inicial',
+      descricao:'Descrição Inicial',
+      cor: '#000 ',
+  } 
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave,valor) {
+    setValue({
+
+      ...values,
+      chave: valor,
+      
+    })
+  }
+
+
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {values.nome}</h1>
+
+
+        <form onSubmit={function handleSubmit(infoDoEvento){
+          infoDoEvento.preventDefault();
+          console.log('Enviando formulario')
+          setCategorias([
+            ...categorias,
+            values,
+          ]);
+        }}>
+          {/* State */}
+
+            <div>
+          <label>
+            Nome da Categoria: 
+            <input
+              type="text"
+              value={values.nome}
+              onChange={function apagaInputInicial (infoDoEvento){
+               setValues(infoDoEvento.target.value)
+              }}
+           />
+          </label>
+            </div>
+
+            <div>
+            <label>
+            Descrição
+            <textarea
+              type="text"
+              value={values.descricao}
+              onChange={function apagaInputInicial (infoDoEvento){
+               setValues(infoDoEvento.target.value)
+              }}
+           />
+          </label>
+            </div>     
+
+            <div>
+            <label>
+            Cor
+            <input
+              type="color"
+              value={values.cor}
+              onChange={function apagaInputInicial (infoDoEvento){
+               setValues(infoDoEvento.target.value)
+              }}
+           />
+          </label>
+            </div>
+
+          <button>
+            Cadastar
+          </button>
+
+        </form>
+        <ul>
+          {categorias.map((categorias, indice) =>{
+            return (
+              <li key={`${categorias}${indice}`}>
+                  {categorias}
+              </li>
+            )
+          })}
+        </ul>
+
+
+
 
       <Link to="/">
         Ir para home
